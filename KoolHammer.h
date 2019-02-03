93bc27a2302a47b131f233da85fc31e2d808a073
Brandon Hahn <blh16b@my.fsu.edu>
2019-02-02 23:58:41 -0500

    Created header file for the Kool Hammer class

---
+++ KoolHammer.h
@@ -0,0 +1,25 @@
+/*
+ * File: KoolHammer.h
+ * Author: Brandon <blh16b@my.fsu.edu
+ *
+ * Created on February 2, 2019
+ */
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef KOOLHAMMER_H
+#define KOOLHAMMER_H
+
+/*
+ * Defines behavior of a kool hammer (hitpoint = 50, if armor < 60, ignores all armor)
+ */
+
+class KoolHammer : public Weapon {
+	public:
+		KoolHammer() : Weapon("Kool hammer", 50.0){};
+		virtual ~KoolHammer(){};
+		virtual double hit(double armor);
+}
+
+#endif
